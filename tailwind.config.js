28f4bbc2bd3cfb2ba4aea3e43a75f5a959396745
fadhilmuhammadilham <fadhilmuhammadilham29@gmail.com>
2025-07-08 11:20:12 +0700

    add branch, update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['./pages/**/*.html', './**/*.js'],
   theme: {
     extend: {
       fontFamily: {
