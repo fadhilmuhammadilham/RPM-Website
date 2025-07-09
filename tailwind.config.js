aec504e7c52c912fe9156af88ef507357f142eea
Naraya Ray <santosoray15@gmail.com>
2025-07-09 23:46:21 +0700

    add contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,12 @@
 module.exports = {
   content: [
     './index.html',
-    './contact.html',
+    './pages/**/*.html',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
-      },
-      screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-        '2xl': '1536px',
       },
     },
   },
