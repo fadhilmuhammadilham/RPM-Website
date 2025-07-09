885a42e4766f92117cf6abd35b3446e7ac25fa87
fadhilmuhammadilham <fadhilmuhammadilham29@gmail.com>
2025-07-09 15:18:21 +0700

    fix style navbar, add scroll logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,19 @@
 module.exports = {
   content: [
     './index.html',
-    './contact.html', // semua HTML dalam folder pages/
+    './contact.html',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
       },
     },
   },
